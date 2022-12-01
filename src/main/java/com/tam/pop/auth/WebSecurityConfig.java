ddb6ad8d9802bea9a2582773162dd0798bcad2f9
Oluwashetemipe <oluwashetemipe@gmail.com>
2022-12-01 20:13:37 +0100

    feat: implement security feature

--- src/main/java/com/tam/pop/auth/WebSecurityConfig.java
+++ src/main/java/com/tam/pop/auth/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.tam.pop.auth;
 
+import com.tam.pop.repository.AdminRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -11,4 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class WebSecurityConfig extends UserDetailsServiceImpl{
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
+
+    public WebSecurityConfig(AdminRepositoryInterface repositoryInterface) {
+        super(repositoryInterface);
+    }
 }
